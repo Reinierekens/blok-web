d64e65e59e73b189e1c7133ff3b3eb48aa630fe7
Reinier <reinier.ekens@live.nl>
2018-02-01 00:28:04 +0100

    update

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,49 @@
-// SELECT ELEMENTS
-var hart = document.querySelector('button.dance');
-var button = document.querySelector('header a img');
+/*jslint browser:true */
 
-// TOGGLE CLASS FUNCTION
-var animatie = function () {
-    hart.classList.toggle('dancing');
-};
+//TOEVOEGEN AAN FAVORIETEN
 
-// ADD EVENT
-button.addEventListener('click', animatie);
+
+
+//var actie = document.querySelector('button.normaal');
+
+//var button = document.querySelector('footer>button.normaal');
+
+//var like = function () {
+
+//actie.classList.toggle('animation');}
+
+
+//Verander bij klik
+
+//button.addEventListener('click', like);
+
+
+// Elemeten selecteren
+
+
+function start() {
+    "use strict";
+
+    var element = document.getElementById("favoriet");
+    element.classList.toggle("animation");
+
+}
+
+
+
+//OFFLINE ZETTEN
+
+//function startDownload() {
+//var uno = document.getElementById("offline");
+//uno.classList.toggle("download");};
+
+var section = document.querySelector('img.imgg'); // first section element
+var button = document.getElementById('offline');
+
+function download() {
+    "use strict";
+    /* 3. toggle een class op een element, dit triggert een CSS Transition (zie CSS: section.dance) */
+    section.classList.toggle('download');
+}
+
+/* 2. voeg een event toe aan een element */
